8623838623a2b7c87aebdf3c80fdbb9e8d5f77c4
Awudu Jamal <78368357+Awudu-Jamal1@users.noreply.github.com>
2024-01-10 16:54:46 +0000

    Update App.jsx

--- ngos/src/App.jsx
+++ ngos/src/App.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router'
 import './App.css'
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
-import Homepage from './components/Home/homePage';
+import Homepage from './components/Home/homepage';
 
 function App() {
   const router = createBrowserRouter([
