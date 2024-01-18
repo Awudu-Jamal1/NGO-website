e0e6d62bb1ecd256fcbbfa821295f45060b9a7a3
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2024-01-18 13:30:14 +0000

    footer in progress

--- ngos/src/components/Footer.jsx
+++ ngos/src/components/Footer.jsx
@@ -1,13 +1,16 @@
+import logo from "../assets/images/logo 2.png"
+
 function Footer() {
     return ( <>
-    <footer className="mx-auto max-w-7xl px-6 lg:px-8 bg-white shadow-lg py-8 text-main">
+
+    <footer className="mx-auto max-w-7xl px-6 lg:px-8 bg-text1 shadow-lg py-8 text-main">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-2 gap-y-16  border-gray-200   lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
             <div
 
               className="flex  text-center  max-w-xl  flex-col items-center justify-between"
             >
-              <h1 className="text-2xl font-bold">LOGO NAME</h1>
+              <img style={{height:'5em'}} src={logo} alt="" />
             </div>
             <div
 
@@ -19,7 +22,10 @@ function Footer() {
 
               className="flex  text-center  max-w-xl  flex-col items-center justify-between"
             >
-              <h1 className=" font-bold ">LOGO NAME</h1>
+              <h1 className=" font-bold ">Follow Us On</h1>
+              <div>
+
+              </div>
             </div>
 
         </div>
