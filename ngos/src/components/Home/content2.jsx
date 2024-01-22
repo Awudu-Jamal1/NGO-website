eec2da31dc10dd324ab233b91f427833fca6af69
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2024-01-22 09:58:48 +0000

    added vision

--- ngos/src/components/Home/content2.jsx
+++ ngos/src/components/Home/content2.jsx
@@ -3,14 +3,25 @@ import Member from "./members";
 
 function Content2() {
     return (  <>
-    <div className="mx-auto max-w-2xl py-10 ">
+    <div className="mx-auto max-w-4xl py-10 ">
 
-<div className="text-center">
-  <h1 className="text-4xl font-bold tracking-tight text-main sm:text-6xl">
+<div className="text-right py-10">
+  <h1 className="text-4xl font-bold tracking-tight text-main sm:text-5xl">
     What We Do
   </h1>
   <p className="mt-6 text-2xl font-bold leading-8 text-text1">
     To inspire millions of individuals around the globe to come up with innovative ideas and solutions to protect and part of the solution.
+  </p>
+
+
+</div>
+<div className="text-left">
+<h1 className="text-4xl font-bold tracking-tight pt-8 text-main sm:text-5xl">
+    Vision
+  </h1>
+  <p className="mt-6 text-2xl font-bold leading-8 text-text1">
+   Through cultural collaboration and education, we seek to create a world
+   where nature would be conserved and our environment protected
   </p>
 </div>
 
